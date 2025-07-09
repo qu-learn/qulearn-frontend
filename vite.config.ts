628802e108970b8b16caf3143e766b62f3f76ecc
Nadeen Udantha <udanthan@gmail.com>
2025-07-09 13:54:25 +0530

    update package and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
