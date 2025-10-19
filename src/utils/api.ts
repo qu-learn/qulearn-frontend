ebf16a23a0dd6598ad0db6d5e6ff6e9478fd5111
Pasindu Ravimal <34201061+PasinduRavimal@users.noreply.github.com>
2025-10-19 21:10:03 +0530

    Feat: Add features to profile settings

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -58,6 +58,8 @@ import type {
     IGetCourseAdministratorResponse,
     IDeleteCourseAdministratorResponse,
     IGetCourseAdministratorsResponse,
+    IChangePasswordRequest,
+    IChangePasswordResponse,
 } from "./types"
 
 // RTK Query API
@@ -366,6 +368,14 @@ export const api = createApi({
                 body: cAdmin,
             }),
         }),
+        changePassword: builder.mutation<IChangePasswordResponse, IChangePasswordRequest>({
+            query: (body) => ({
+                url: "/users/me/change-password",
+                method: "PATCH",
+                body,
+            }),
+            invalidatesTags: ["User"],
+        }),
     }),
 })
 
@@ -416,4 +426,5 @@ export const {
     useDeleteCourseAdministratorMutation,
     useGetCourseAdministratorQuery,
     useUpdateCourseAdministratorMutation,
+    useChangePasswordMutation,
 } = api
